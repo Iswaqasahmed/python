1b205226489bd46299796f8189f66bb812f8490f
waqas ahmed <37307194+Iswaqasahmed@users.noreply.github.com>
2020-11-10 00:00:40 +0500

    Update regexp.py

--- Regular Expression/regexp.py
+++ Regular Expression/regexp.py
@@ -1,1 +1,4 @@
 import re # imported regular expression module
+phoneNumber = re.compile('r(\d{4}-\d{4}-\d{3}')
+search  = phoneNumber.search('My Mobile Number is 0311-3678-238')
+print("my number is founded ",search)
