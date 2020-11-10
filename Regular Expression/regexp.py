79968efa4310857e590ea4ba3f192016912404b8
waqas ahmed <37307194+Iswaqasahmed@users.noreply.github.com>
2020-11-10 23:36:55 +0500

    somechanges

--- Regular Expression/regexp.py
+++ Regular Expression/regexp.py
@@ -1,4 +1,20 @@
 import re # imported regular expression module
-phoneNumber = re.compile(r'(\d{4}-\d{4}-\d{3})')
-search  = phoneNumber.search('My Mobile Number is 0311-3678-44465238')
-print("my number is founded ",search.group())
+
+class Regexp:    
+    # def __init__(self):
+    #     self.text = " ";
+    def mobileParse(self,text):
+        phoneNumber = re.compile('\d{4}-\d{4}-\d{3}')
+        search  = phoneNumber.search(text)
+        print(text)
+        print("my number is founded ",search.group())
+
+
+
+
+        
+
+
+
+reg = Regexp()
+reg.mobileParse('0341-257866546446')
