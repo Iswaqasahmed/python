0b35324b7d777688d4a44504836b38c6a44e596b
waqas ahmed <37307194+Iswaqasahmed@users.noreply.github.com>
2020-11-10 22:20:07 +0500

    Update regexp.py

--- Regular Expression/regexp.py
+++ Regular Expression/regexp.py
@@ -1,4 +1,4 @@
 import re # imported regular expression module
-phoneNumber = re.compile('r(\d{4}-\d{4}-\d{3}')
-search  = phoneNumber.search('My Mobile Number is 0311-3678-238')
-print("my number is founded ",search)
+phoneNumber = re.compile(r'(\d{4}-\d{4}-\d{3})')
+search  = phoneNumber.search('My Mobile Number is 0311-3678-44465238')
+print("my number is founded ",search.group())
